b4dc816505cb75950536e9fcb863929b3653ce1e
samvrny <samvrny@gmail.com>
2023-03-22 12:34:59 -0500

    got all images printing to the screen

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//global variables
+const scrollContainer = document.getElementById('scroll-box');
+const loaderSpinner = document.getElementById('loader');
+let imagesArray = [];
+
 //Unsplash API information
 const count = 10;
 const apiKey = 'JHdBkpLAKEW0BaEth6oAFDZWSGf7tvr5xtucle_jW6I';
@@ -7,11 +12,30 @@ const unsplashApiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKe
 async function fetchImages() {
     try {
         const response = await fetch(unsplashApiUrl);
-        const data = await response.json();
-        console.log(data)
+        imagesArray = await response.json();
+        displayImages();
     } catch (error) {
         //Catch error here
     }
+}
+
+//print images to the screen
+function displayImages() {
+    console.log(imagesArray);
+    imagesArray.forEach((image) => {
+        const anchor = document.createElement('a');
+        anchor.setAttribute('href', image.links.html);
+        anchor.setAttribute('target', '_blank');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', image.urls.regular);
+        img.setAttribute('alt', image.alt_description);
+        img.setAttribute('title', image.alt_description);
+
+        // anchor.appendChild(img);
+        scrollContainer.appendChild(anchor);
+        anchor.appendChild(img);
+    });
 }
 
 fetchImages();
